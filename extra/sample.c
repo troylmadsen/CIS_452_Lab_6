78bd2736fc6467a0b4e905cf020d7a407762bef2
Troy Madsen <madsentr@mail.gvsu.edu>
2018-10-11 13:09:48 -0400

    -make clean update -Removed unnecessary dependencies in sample.c

--- extra/sample.c
+++ extra/sample.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ipc.h>
-#include <sys/sem.h>
 #include <sys/shm.h>
-#include <sys/stat.h>
 
 #include "sem_tk.h"
 
